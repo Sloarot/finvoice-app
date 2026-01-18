e862ac93b827bccfae3202214a940a9e9ae9cb87
Sloarot <xiaomistijn@gmail.com>
2026-01-18 14:50:53 -0500

    final update, ready for prdocution

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,11 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    // Registration routes disabled - single user system
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //     ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
